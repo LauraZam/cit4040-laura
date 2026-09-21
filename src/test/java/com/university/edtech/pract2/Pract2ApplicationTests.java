921def0c92ce498779593ea26135e7f26509d794
laurazam <77395682+LauraZam@users.noreply.github.com>
2026-09-21 09:36:25 +0500

    Add the Hello program that prints my name

---
+++ src/test/java/com/university/edtech/pract2/Pract2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.edtech.pract2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
